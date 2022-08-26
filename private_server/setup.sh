6be75a203908f398c6531d3d6afea2a82dabd6da
RewardedIvan <67065165+RewardedIvan@users.noreply.github.com>
2022-08-26 17:17:40 +0300

    no https://

--- private_server/setup.sh
+++ private_server/setup.sh
@@ -2,7 +2,7 @@
 cd /3dps
 #openssl req -x509 -nodes -newkey rsa:2048 -keyout TLS.key -out TLS.crt -days 69420 -subj "/C=EA/ST=Planet/L=Earth/O=Global Security/OU=IT Department/CN=3DPS"
 go run 3dps.go
-go mod init https://github.com/RewardedIvan/3DPS
+go mod init github.com/RewardedIvan/3DPS
 go mod tidy
 go install github.com/mattn/go-sqlite3@latest
 go run 3dps.go
